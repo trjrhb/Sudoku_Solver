b99b1a8448be668ce0de416276fddc2d404880eb
Tyler Jones <trjrhb@umsystem.edu>
2020-10-01 09:09:44 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+sodoku = [[3,0,0,8,0,1,0,0,2],
+          [2,0,1,0,3,0,6,0,4],
+          [0,0,0,2,0,4,0,0,0],
+          [8,0,9,0,0,0,1,0,6],
+          [0,6,0,0,0,0,0,5,0],
+          [7,0,2,0,0,0,4,0,9],
+          [0,0,0,5,0,9,0,0,0],
+          [9,0,4,0,8,0,7,0,5],
+          [6,0,0,1,0,7,0,0,3]]
+
